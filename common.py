09f527aabc9f5848bc03f6e898c21469773928ba
hpuissant <hpuissant@cyres.fr>
2016-07-05 14:53:39 +0200

    delete old .py files

--- common.py
+++
@@ -1,9 +0,0 @@
-import os
-import os.path
-
-def checkConfigFile(path):
-    if not os.path.isfile(path):
-        print "Config file not found"
-
-def get_first_broker(brokers):
-    return brokers.split(",")[0].split(":")[0]
